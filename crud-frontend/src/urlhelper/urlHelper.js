d4a2400ca813216d02d0574dad94dd4043a9dd11
Om Jadav <omjadav04@gmail.com>
2024-01-28 01:56:16 +0530

    update urlHelper

--- crud-frontend/src/urlhelper/urlHelper.js
+++ crud-frontend/src/urlhelper/urlHelper.js
@@ -1,3 +1,3 @@
-const backendUrl = "http://localhost:5000"
-//  "https://mern-crud-sdgb.onrender.com"
+const backendUrl = "https://mern-crud-sdgb.onrender.com"
+//  "http://localhost:5000"
 module.exports = backendUrl;
